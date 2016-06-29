928260b198e622a38996c9f87e6c601e179ec5ee
Marco Kaul <marco.kaul@ueberbit.de>
2016-06-29 19:18:31 +0200

    Add inactive style for past runs, active line and active run

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import Run from './Run';
+import moment from 'moment';
 import '../styles/Day.scss';
 
+const TWENTYFOUR = moment.duration(24, 'hours');
+const NOW = moment().month(6).date(5);
+
 export default class Day extends Component {
+  componentDidMount() {
+    if (this.refs.now) {
+      this.refs.now.scrollIntoView();
+    }
+  }
+
   render() {
     let {date, runs} = this.props;
+    let now = false;
+
+    if (NOW.date() == date.date()) {
+      now = true;
+    }
 
     return (<div className="day">
       <header className="day__header">
         {date.format('dddd Do')}
       </header>
       <div className="day__content">
+        {now && <div ref="now" className="day__now" style={{top: 100 * NOW.diff(date.startOf('day')) / TWENTYFOUR + '%'}}></div>}
         {runs.map((run) => <Run
           key={run.timestamp}
           name={run.name}
--- src/components/Run.js
+++ src/components/Run.js
@@ -3,6 +3,9 @@ import moment from 'moment';
 import '../styles/Run.scss';
 
 const twentyfour = moment.duration(24, 'hours');
+const NOW = moment().month(6).date(5);
+
+console.log(NOW.format('L'))
 
 export default class Run extends Component {
   render() {
@@ -13,7 +16,17 @@ export default class Run extends Component {
     let positionPercentage = 100 * start.diff(day) / twentyfour;
     let lengthPercentage = 100 * end.diff(start) / twentyfour;
 
-    return (<div className={`run${startedYesterday ? ' run--continuing' : ''}${endsTomorrow ? ' run--ending' : ''}`}
+    let status = null;
+
+    if (timestamp < NOW) {
+      status = 'inactive';
+
+      if (NOW < moment(timestamp).add(duration)) {
+        status = 'highlight';
+      }
+    }
+
+    return (<div className={`run${startedYesterday ? ' run--continuing' : ''}${endsTomorrow ? ' run--ending' : ''}${status ? ' run--' + status : ''}`}
                  style={{top: positionPercentage + '%', height: `calc(${lengthPercentage}% - 10px)`}}>
       <div className="run__time">
         <span className="run__starttime">{timestamp.format('LT')}</span>
