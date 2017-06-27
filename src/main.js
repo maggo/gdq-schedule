8be026703f6251b8d490bc01e7f6a11e6b3d4029
Marco Kaul <marcokaul@googlemail.com>
2017-06-27 18:50:57 +0200

    Fix YQL api call

--- src/main.js
+++ src/main.js
@@ -68,7 +68,7 @@ const getData = () => {
     resolve(data);
   });
 
-  request.open("GET", "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fgamesdonequick.com%2Fschedule%22%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22runTable%22%5D%2Ftbody%2Ftr'&format=json&callback=");
+  request.open("GET", "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20htmlstring%20where%20url%3D%22https%3A%2F%2Fgamesdonequick.com%2Fschedule%22%20and%20xpath%3D%27%2F%2F*%5B%40id%3D%22runTable%22%5D%2Ftbody%2Ftr%27&format=json&callback=&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
   request.send();
 };
 
