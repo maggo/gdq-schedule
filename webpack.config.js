98b65692b3c00d12a9075d701cc5335c1a0ad713
maggo <marcokaul@gmail.com>
2020-01-07 19:16:13 +0100

    Update dependencies and remove unused files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = env => {
   return {
